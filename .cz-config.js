9669f0314b4df90dd4ac7a6a948a774e64f32fda
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-12-04 15:59:55 +0900

    ✨(env): add git-cz

---
+++ .cz-config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  types: [
+    { value: '✨', name: 'New function' },
+    { value: '🔧', name: 'bug fix' },
+    { value: '😍', name: 'fix' },
+    { value: '💪', name: 'other' }
+  ]
+};
+
